c74879bd6cf896cc5a125caae37776315d5c98dc
Molkobain <lajarige.guillaume@free.fr>
2019-02-01 22:59:14 +0100

    :rotating_light: Warning suppression

--- dist/molkobain-bubble-caselogs/module.molkobain-bubble-caselogs.php
+++ dist/molkobain-bubble-caselogs/module.molkobain-bubble-caselogs.php
@@ -11,6 +11,7 @@
 // iTop module definition file
 //
 
+/** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
 	'molkobain-bubble-caselogs/1.0.1',
