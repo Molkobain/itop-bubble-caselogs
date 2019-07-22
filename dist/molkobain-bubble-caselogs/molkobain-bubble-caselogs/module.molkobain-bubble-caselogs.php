d99301be82e4f50741a1cd646df6282f5022e147
Molkobain <lajarige.guillaume@free.fr>
2019-07-22 21:49:40 +0200

    :package: Update redistribuables

--- dist/molkobain-bubble-caselogs/molkobain-bubble-caselogs/module.molkobain-bubble-caselogs.php
+++ dist/molkobain-bubble-caselogs/molkobain-bubble-caselogs/module.molkobain-bubble-caselogs.php
@@ -14,7 +14,7 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'molkobain-bubble-caselogs/1.1.4',
+	'molkobain-bubble-caselogs/1.2.0',
 	array(
 		// Identification
 		//
@@ -24,8 +24,9 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			'molkobain-handy-framework/1.1.0',
+			'molkobain-handy-framework/1.2.4',
 			'molkobain-fontawesome5-pack/1.1.1',
+			'molkobain-newsroom-provider/1.0.0',
 		),
 		'mandatory' => false,
 		'visible' => true,
