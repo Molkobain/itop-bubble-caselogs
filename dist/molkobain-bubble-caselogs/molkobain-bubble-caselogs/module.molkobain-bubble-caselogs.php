3582e4bef0b06e1a56d50294c88ccc282b84f4f7
Molkobain <lajarige.guillaume@free.fr>
2020-04-02 22:55:19 +0200

    :package: Update redistributes

--- dist/molkobain-bubble-caselogs/molkobain-bubble-caselogs/module.molkobain-bubble-caselogs.php
+++ dist/molkobain-bubble-caselogs/molkobain-bubble-caselogs/module.molkobain-bubble-caselogs.php
@@ -14,7 +14,7 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'molkobain-bubble-caselogs/1.3.2',
+	'molkobain-bubble-caselogs/1.3.3',
 	array(
 		// Identification
 		//
